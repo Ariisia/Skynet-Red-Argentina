621f5eba03ab2d5a9ad38522ff32fa48eba1ab03
Agus <alexisthe14th@hotmail.com>
2019-06-01 13:54:00 -0300

    Iniciando el proyecto.

---
+++ Skynet-Red-Argentina/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skynet_Red_Argentina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
