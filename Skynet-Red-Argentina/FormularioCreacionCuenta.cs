72e69abbd5cd7e47bccf9dc5c10ff6df4c61675f
Agus <alexisthe14th@hotmail.com>
2019-06-01 16:16:16 -0300

    Se creo el mensaje de Bienvenida y pregunta por cuenta. Se agrego el formulario de creacion de cuenta.

---
+++ Skynet-Red-Argentina/FormularioCreacionCuenta.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skynet_Red_Argentina
+{
+    class FormularioCreacionCuenta
+    {
+        public void CrearCuenta()
+        {
+            string nombre, apellido, docNTipo, docTipo = "", docNumero = "", direccion, provincia, ciudad, usuario, contraseña, verificacion;
+            Console.Clear();
+            Console.WriteLine("A continuación debera crear su cuenta Skynet.");
+
+            Console.Clear();
+
+            Console.WriteLine("Ingrese su nombre.");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su apellido.");
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("Seleccione el tipo documento:");
+            Console.WriteLine("1 DNI");
+            Console.WriteLine("2 Cedula de Identidad");
+            Console.WriteLine("3 Libreta de Enrolamiento");
+            Console.WriteLine("4 Libreta Cívica");
+            docNTipo = Console.ReadLine();
+
+            Console.Clear();
+
+            while (docNTipo != "1" && docNTipo != "2" && docNTipo != "3" && docNTipo != "4")
+            {
+                Console.WriteLine("No selecciono una entrada valida.");
+                Console.WriteLine("Seleccione el tipo documento:");
+                Console.WriteLine("1 DNI");
+                Console.WriteLine("2 Cedula de Identidad");
+                Console.WriteLine("3 Libreta de Enrolamiento");
+                Console.WriteLine("4 Libreta Cívica");
+                docNTipo = Console.ReadLine();
+            }
+            Console.Clear();
+
+            switch (docNTipo)
+            {
+                case "1":
+                    Console.WriteLine("Ingrese su numero de DNI");
+                    docTipo = "DNI";
+                    docNumero = Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("Ingrese su numero de Cedula de Identidad");
+                    docNumero = Console.ReadLine();
+                    docTipo = "Cedula de Identidad";
+                    break;
+                case "3":
+                    Console.WriteLine("Ingrese su numero de Libreta de Enrolamiento");
+                    docNumero = Console.ReadLine();
+                    docTipo = "Libreta de Enrolamiento";
+                    break;
+                case "4":
+                    Console.WriteLine("Ingrese su numero de Libreta Cívica");
+                    docNumero = Console.ReadLine();
+                    docTipo = "Libreta Cívica";
+                    break;
+            }
+            Console.Clear();
+
+            Console.WriteLine("Indique su direccion, numero, depto y piso (si fuera necesario).");
+            direccion = Console.ReadLine();
+
+            Console.WriteLine("Indique su provincia.");
+            provincia = Console.ReadLine();
+
+            Console.WriteLine("Indique su ciudad");
+            ciudad = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Ingrese su nuevo usuario.");
+            usuario = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Ingrese su nueva contraseña.");
+            contraseña = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Repita su nueva contraseña.");
+            verificacion = Console.ReadLine();
+            Console.Clear();
+
+            while (contraseña != verificacion)
+            {
+                Console.Clear();
+                Console.WriteLine("Las contraseñas no coinciden, intentelo de nuevo.");
+                Console.WriteLine("Ingrese su nueva contraseña.");
+                contraseña = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Repita su nueva contraseña.");
+                verificacion = Console.ReadLine();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Su cuenta ha sido creada exitosamente, presione tecla Enter para continuar.");
+            Console.ReadLine();
+        }
+    }
+}
--- Skynet-Red-Argentina/Program.cs
+++ Skynet-Red-Argentina/Program.cs
@@ -10,6 +10,30 @@ namespace Skynet_Red_Argentina
     {
         static void Main(string[] args)
         {
+            FormularioCreacionCuenta formulario = new FormularioCreacionCuenta();
+            string cuenta;
+            Console.WriteLine("Bienvenido a la red Argentina de cajeros Skynet.");
+            Console.WriteLine();
+            Console.WriteLine("Para operar con esta terminal seleccione el numero de la opcion deseada y presione la tecla Enter.");
+            Console.WriteLine();
+            Console.WriteLine("¿Posee ya una cuenta Skynet?");
+            Console.WriteLine("1 Si");
+            Console.WriteLine("2 No");
+            cuenta = Console.ReadLine();
+
+            while (Convert.ToInt32(cuenta) != 1 && Convert.ToInt32(cuenta) != 2)
+            {
+                Console.WriteLine("Por favor seleccione una opcion valida.");
+                Console.WriteLine("¿Posee ya una cuenta Skynet?");
+                Console.WriteLine("1 Si");
+                Console.WriteLine("2 No");
+                cuenta = Console.ReadLine();
+            }
+
+            if (Convert.ToInt32(cuenta) == 2)
+            {
+                formulario.CrearCuenta();
+            }
         }
     }
 }
